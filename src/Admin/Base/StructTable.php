8340d3aff5a3cd14c46b1f9d983b253769a2bf7b
Denis Glushkov <amkarovec@gmail.com>
2020-10-18 21:05:19 +0500

    WIP Initial

---
+++ src/Admin/Base/StructTable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Simplex\Admin\Base;
+
+
+use Simplex\Admin\Base;
+use Simplex\Admin\Page;
+use Simplex\Core\DB;
+
+class StructTable extends Base {
+    
+    public function __construct() {
+        parent::__construct();
+        Page::js('/admin/base/js/struct.table.js');
+    }
+
+    protected function portlets($position = 'right') {
+        parent::portlets($position);
+        if ('right' == $position) {
+            if (isset($this->row['table_id'])) {
+
+                if ($this->ids) {
+                    
+                } else {
+
+                    $q = "SELECT * FROM user_role order by role_id";
+                    $roles = DB::assoc($q);
+
+                    $q = "SELECT * FROM struct_table_right WHERE table_id = {$this->row['table_id']}";
+                    $rights = DB::assoc($q, 'role_id');
+
+                    include 'tpl/struct_table/rights.tpl';
+                }
+            }
+        }
+    }
+
+    public function save() {
+        if ($this->ids) {
+            return $this->saveGroup();
+        }
+
+        $ret = parent::save();
+        $pkValue = $_POST[$this->pk->name];
+
+        $q = "DELETE FROM struct_table_right WHERE table_id = $pkValue";
+        DB::query($q);
+
+        if ($rights = @$_POST['rights']) {
+            foreach ($rights as $roleId => $roleRights) {
+                $q = "
+                    INSERT INTO struct_table_right SET 
+                    table_id = $pkValue, role_id = $roleId,
+                    can_add = " . (int) isset($roleRights['can_add']) . ", 
+                    can_edit = " . (int) isset($roleRights['can_edit']) . ", 
+                    can_delete = " . (int) isset($roleRights['can_delete']) . "
+                ";
+                DB::query($q);
+            }
+        }
+
+        return $ret;
+    }
+
+}
---
+++ src/Admin/index.php
@@ -0,0 +1,41 @@
+<?php
+
+define('SF_INADMIN', true);
+define('SF_INSITE', false);
+define('SF_INCRON', false);
+
+$m0 = memory_get_usage();
+$time_start = microtime();
+session_start();
+
+//date_default_timezone_set('Europe/Moscow');
+date_default_timezone_set('Asia/Yekaterinburg');
+
+ini_set('default_charset', 'UTF-8');
+
+include '../autoload.php';
+include 'autoload.php';
+
+include '../config.php';
+include '../core/sflog.class.php';
+include '../core/sfcore.class.php';
+include '../core/sfpage.class.php';
+include '../core/sfdb.class.php';
+include '../core/sfuser.class.php';
+include '../core/sffile.class.php';
+include '../core/sfservice.class.php';
+include '../core/sfextbase.class.php';
+include '../core/sfcombase.class.php';
+include '../core/sfmodbase.class.php';
+include '../core/sfbuffer.class.php';
+include 'base/base.class.php';
+include 'base/struct.class.php';
+include 'core/sfadmincore.class.php';
+include 'core/sfadminpage.class.php';
+
+
+
+DB::connect();
+SFUser::login('admin');
+SFAdminCore::init();
+SFAdminCore::execute();
