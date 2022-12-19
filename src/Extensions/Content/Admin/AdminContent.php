95e55b2033584f858e288d7c15648a93f6641856
Denis Glushkov <amkarovec@gmail.com>
2022-12-19 15:23:13 +0500

    content template sort params

--- src/Extensions/Content/Admin/AdminContent.php
+++ src/Extensions/Content/Admin/AdminContent.php
@@ -11,17 +11,18 @@ class AdminContent extends Base
     {
         $contentId = (int)($_REQUEST[$this->pk->name] ?? 0);
         $q = "
-            SELECT param_id, param_pid, pos, t1.name, t1.label, t1.params, t2.class, '$this->table' `table`, null default_value
+            SELECT param_id, param_pid, pos, t1.name, t1.label, t1.params, t2.class, '$this->table' `table`, null default_value, 0 npp
             FROM struct_param t1
             LEFT JOIN struct_field t2 USING(field_id)
             WHERE table_id = $this->tableId
             UNION ALL
             SELECT ctp_id + 1000000 as param_id, param_pid, position as pos,
-                   t1.name, t1.label, t1.params, t2.class, '$this->table' `table`, default_value
+                   t1.name, t1.label, t1.params, t2.class, '$this->table' `table`, default_value, npp
             FROM content_template_param t1
             JOIN content c USING(template_id)
             LEFT JOIN struct_field t2 USING(field_id)
             WHERE c.content_id = $contentId
+            ORDER BY npp, label
         ";
         $params = DB::assoc($q, 'param_pid', 'param_id');
         return $params;
