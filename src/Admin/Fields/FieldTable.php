34d05751af1fb96a58fc3cd32009017602d7a74a
panzerfaust <panzerfaust@ev0lve.xyz>
2023-03-15 17:50:54 +0500

    Small code refactor

--- src/Admin/Fields/FieldTable.php
+++ src/Admin/Fields/FieldTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Admin\Fields;
 
 use Simplex\Admin\Fields\Field;
@@ -20,7 +21,8 @@ class FieldTable extends Field
         }
 
         $tmpVal = json_decode($value, true);
-        if (!str_contains(Container::getRequest()->getPath(), 'content_template_param') && $this->params['struct'] && $tmpVal['s'] != $this->params['struct']) {
+        if (!str_contains(Container::getRequest()->getPath(), 'content_template_param') &&
+            $this->params['struct'] && $tmpVal['s'] != $this->params['struct']) {
             $s = json_decode($this->params['struct'], true);
             $v = json_decode($value, true);
 
@@ -39,24 +41,8 @@ class FieldTable extends Field
         // generate id for the js
         $jsId = (string)crc32($this->inputName());
 
-        // output the anchor
-        $html = '<input data-id="' . $jsId . '" type="hidden" name="' . $this->inputName() . '" value="' . htmlspecialchars($value) . '" />';
-
-        // rendering will be performed on the frontend.
-        // display base area and basic control buttons here.
-
-        $html .= '<div data-id="' . $jsId . '" data-field="' . $this->name . '">';
-        $html .= '<div>';
-        $html .= '<div>
-    <button class="btn btn-default btn-sm" onclick="TableEditor.onRowEdit(\''.$jsId.'\', null);return false;"><i class="fa fa-plus"></i> Добавить строку</button>
-</div>';
-        $html .= '</div>';
-        $html .= '<div style="overflow: auto">';
-        $html .= '<table class="table"><tbody><tr class="heading"></tr></tbody></table>';
-        $html .= '</div>';
-        $html .= '</div>';
-        $html .= '<script>TableEditor.init("' . $jsId . '");</script>';
-
-        return $html;
+        ob_start();
+        include '../theme/tpl/fields/table.tpl';
+        return ob_get_clean();
     }
 }
